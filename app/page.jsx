b7df7b37ac23762ac60a049dbd8e12618e13eee4
MostakemHossain <mostakememon123@gmail.com>
2025-06-10 22:06:24 +0600

    testimonial section

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Banner from "@/components/Banner";
 import HowItWorks from "@/components/how-it-works";
 import Pricing from "@/components/pricing";
+import Testimonial from "@/components/testimonial";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Banner/>
      <HowItWorks/>
      <Pricing/>
+     <Testimonial/>
     </div>
   );
 }
